72947ab980e063d5256ee0a296d5e5e3847b6c3b
sudmat <dingjiesu@163.com>
2019-08-04 22:27:32 -0500

    1 update config and requirements.txt

--- start_worker.py
+++ start_worker.py
@@ -1,8 +1,10 @@
 from node.work_node import WorkNode
 from configobj import ConfigObj
+import sys
 
-
-name = 'worker1'
+name = 'worker0'
+if len(sys.argv) >= 2:
+    name = sys.argv[1]
 config = ConfigObj('config/test.ini')
 config = config[name]
 
